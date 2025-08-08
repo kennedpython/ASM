0c4e0364dfed88e197e2848caf285a89d5e5299b
kennedpython <kennedymiranda@hotmail.com>
2025-08-08 09:34:40 -0300

    Commit inicial

--- main.asm
+++ main.asm
@@ -1,1 +1,12 @@
-"section .text" 
+section .data
+    msg db "Hello, World!", 10, 0  ; string + newline + null terminator
+
+section .text
+    global main
+    extern printf
+
+main:
+    push msg
+    call printf
+    add esp, 4
+    ret
